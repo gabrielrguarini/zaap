c0ce32ac1a0ff3c5d5571d84de57edfb251e6a38
Gabriel Guarini <gabrielrguarini@gmail.com>
2025-01-11 11:42:36 -0300

    feat: adjust gallery header layout

--- src/app/_components/galery/_components/galery-header.tsx
+++ src/app/_components/galery/_components/galery-header.tsx
@@ -2,8 +2,8 @@ import GalerySearch from "./galery-search";
 
 const GaleryHeader = () => {
   return (
-    <div className="my-10 flex justify-between gap-4 p-2 sm:m-0 md:gap-8 md:p-8">
-      <h2 className="block whitespace-nowrap bg-gradient-to-r from-[#ffb400] to-[#ff4800] bg-clip-text text-3xl font-semibold text-transparent">
+    <div className="my-10 flex justify-between p-2 sm:m-0 sm:gap-4 md:gap-8 md:p-8">
+      <h2 className="block whitespace-nowrap bg-gradient-to-r from-[#ffb400] to-[#ff4800] bg-clip-text text-3xl font-semibold text-transparent sm:mr-0">
         Galeria de fotos
       </h2>
       <GalerySearch />
--- src/app/_components/galery/_components/galery-search.tsx
+++ src/app/_components/galery/_components/galery-search.tsx
@@ -1,16 +1,23 @@
 import Image from "next/image";
+import { useState } from "react";
 
 const GalerySearch = () => {
+  const [search, setSearch] = useState("");
   return (
-    <div className="ml-auto min-w-24 rounded-full bg-gradient-to-r from-[#ffb400] to-[#ff4800] p-[2px] sm:min-w-72">
+    <div className="ml-auto w-full max-w-[50%] rounded-full bg-gradient-to-r from-[#ffb400] to-[#ff4800] p-[2px] sm:w-auto sm:min-w-72">
       <div className="relative h-8 w-full rounded-full bg-background">
         <input
-          className="hidden h-full w-full bg-transparent px-2 outline-none sm:block"
+          className="outline-non absolute inset-0 shrink-0 bg-transparent px-2"
+          type="text"
           placeholder="Ex: Casamento João"
-        ></input>
-        <div className="absolute right-4 top-1 flex items-center">
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className="pointer-events-none absolute right-0 top-1 flex items-center rounded-full bg-background pl-1 pr-4">
           <Image src={"/search.png"} width={20} height={20} alt={"Search"} />
-          <p className="ml-2 mt-1 text-xs font-semibold">Buscar</p>
+          <p className="ml-2 mt-1 hidden text-xs font-semibold sm:block">
+            Buscar
+          </p>
         </div>
       </div>
     </div>
