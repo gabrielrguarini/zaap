c6a6a57db9c696d52d257498eaf1a2ba46ae9de9
Gabriel Guarini <gabrielrguarini@gmail.com>
2025-01-11 13:29:32 -0300

    feat: add focus and blur to gallery header

--- src/app/_components/galery/_components/galery-header.tsx
+++ src/app/_components/galery/_components/galery-header.tsx
@@ -1,12 +1,25 @@
 import GalerySearch from "./galery-search";
+import { useState } from "react";
 
 const GaleryHeader = () => {
+  const [isInputFocused, setIsInputFocused] = useState(false);
+
+  const handleFocus = () => {
+    setIsInputFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsInputFocused(false);
+  };
+
   return (
     <div className="my-10 flex justify-between p-2 sm:m-0 sm:gap-4 md:gap-8 md:p-8">
-      <h2 className="block whitespace-nowrap bg-gradient-to-r from-[#ffb400] to-[#ff4800] bg-clip-text text-3xl font-semibold text-transparent sm:mr-0">
-        Galeria de fotos
-      </h2>
-      <GalerySearch />
+      {!isInputFocused && (
+        <h2 className="mr-2 whitespace-nowrap bg-gradient-to-r from-[#ffb400] to-[#ff4800] bg-clip-text text-3xl font-semibold text-transparent sm:mr-0">
+          Galeria de fotos
+        </h2>
+      )}
+      <GalerySearch onFocus={handleFocus} onBlur={handleBlur} />
     </div>
   );
 };
--- src/app/_components/galery/_components/galery-search.tsx
+++ src/app/_components/galery/_components/galery-search.tsx
@@ -1,17 +1,25 @@
 import Image from "next/image";
 import { useState } from "react";
 
-const GalerySearch = () => {
+interface GalerySearchProps {
+  onFocus: () => void;
+  onBlur: () => void;
+}
+
+const GalerySearch = ({ onFocus, onBlur }: GalerySearchProps) => {
   const [search, setSearch] = useState("");
+
   return (
-    <div className="ml-auto w-full max-w-[50%] rounded-full bg-gradient-to-r from-[#ffb400] to-[#ff4800] p-[2px] sm:w-auto sm:min-w-72">
+    <div className="ml-auto w-full rounded-full bg-gradient-to-r from-[#ffb400] to-[#ff4800] p-[2px] sm:w-auto sm:min-w-72">
       <div className="relative h-8 w-full rounded-full bg-background">
         <input
-          className="outline-non absolute inset-0 shrink-0 bg-transparent px-2"
+          className="absolute inset-0 shrink-0 bg-transparent px-2 outline-none"
           type="text"
           placeholder="Ex: Casamento João"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
+          onFocus={onFocus}
+          onBlur={onBlur}
         />
         <div className="pointer-events-none absolute right-0 top-1 flex items-center rounded-full bg-background pl-1 pr-4">
           <Image src={"/search.png"} width={20} height={20} alt={"Search"} />
