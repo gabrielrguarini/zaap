584176df9daf4f2a829a710f58b2d1f343ffe359
Gabriel Guarini <gabrielrguarini@gmail.com>
2025-01-13 22:20:25 -0300

    feat: refactor gallery header and search components for input focus handling

--- src/app/_components/galery/_components/galery-header.tsx
+++ src/app/_components/galery/_components/galery-header.tsx
@@ -4,22 +4,17 @@ import { useState } from "react";
 const GaleryHeader = () => {
   const [isInputFocused, setIsInputFocused] = useState(false);
 
-  const handleFocus = () => {
-    setIsInputFocused(true);
-  };
-
-  const handleBlur = () => {
-    setIsInputFocused(false);
-  };
-
   return (
     <div className="my-10 flex justify-between p-2 sm:m-0 sm:gap-4 md:gap-8 md:p-8">
-      {!isInputFocused && (
-        <h2 className="mr-2 whitespace-nowrap bg-gradient-to-r from-[#ffb400] to-[#ff4800] bg-clip-text text-3xl font-semibold text-transparent sm:mr-0">
-          Galeria de fotos
-        </h2>
-      )}
-      <GalerySearch onFocus={handleFocus} onBlur={handleBlur} />
+      <h2
+        className={`${isInputFocused && "hidden"} mr-2 whitespace-nowrap bg-gradient-to-r from-[#ffb400] to-[#ff4800] bg-clip-text text-3xl font-semibold text-transparent sm:mr-0 sm:flex`}
+      >
+        Galeria de fotos
+      </h2>
+      <GalerySearch
+        isInputFocused={isInputFocused}
+        setIsInputFocused={setIsInputFocused}
+      />
     </div>
   );
 };
--- src/app/_components/galery/_components/galery-search.tsx
+++ src/app/_components/galery/_components/galery-search.tsx
@@ -2,30 +2,33 @@ import Image from "next/image";
 import { useState } from "react";
 
 interface GalerySearchProps {
-  onFocus: () => void;
-  onBlur: () => void;
+  isInputFocused: boolean;
+  setIsInputFocused: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const GalerySearch = ({ onFocus, onBlur }: GalerySearchProps) => {
+const GalerySearch = ({
+  isInputFocused,
+  setIsInputFocused,
+}: GalerySearchProps) => {
   const [search, setSearch] = useState("");
 
   return (
-    <div className="ml-auto w-full rounded-full bg-gradient-to-r from-[#ffb400] to-[#ff4800] p-[2px] sm:w-auto sm:min-w-72">
-      <div className="relative h-8 w-full rounded-full bg-background">
+    <div
+      className={`ml-auto rounded-full bg-gradient-to-r from-[#ffb400] to-[#ff4800] p-[2px] ${isInputFocused ? "w-screen" : "w-72"} transition-all duration-300`}
+    >
+      <div className="relative h-8 rounded-full bg-background">
         <input
           className="absolute inset-0 shrink-0 bg-transparent px-2 outline-none"
           type="text"
           placeholder="Ex: Casamento João"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          onFocus={onFocus}
-          onBlur={onBlur}
+          onFocus={() => setIsInputFocused(true)}
+          onBlur={() => setIsInputFocused(false)}
         />
         <div className="pointer-events-none absolute right-0 top-1 flex items-center rounded-full bg-background pl-1 pr-4">
           <Image src={"/search.png"} width={20} height={20} alt={"Search"} />
-          <p className="ml-2 mt-1 hidden text-xs font-semibold sm:block">
-            Buscar
-          </p>
+          <p className="ml-2 mt-1 text-xs font-semibold">Buscar</p>
         </div>
       </div>
     </div>
