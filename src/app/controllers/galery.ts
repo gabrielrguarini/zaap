dbc9322f41aca1faea32d9e840ebfb2b89057cbd
Gabriel Guarini <gabrielrguarini@gmail.com>
2025-02-01 21:25:33 -0300

    feat: remove console.log from application

--- src/app/controllers/galery.ts
+++ src/app/controllers/galery.ts
@@ -4,13 +4,12 @@ import { prisma } from "@/utils/prisma";
 
 export async function createGalery(formData: FormData) {
   const title = formData.get("title") as string;
-  const galery = await prisma.galery.create({
+  await prisma.galery.create({
     data: {
       title,
       authorId: "1",
     },
   });
-  return galery;
 }
 
 export async function getGalery({ authorId }: { authorId: string }) {
