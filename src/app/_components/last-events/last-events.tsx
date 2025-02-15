2562434115afc00ba40aa61d8f8a2c09b07564f1
Gabriel Guarini <gabrielrguarini@gmail.com>
2025-02-15 13:47:51 -0300

    feat: refactor LastEvents component to use custom hook for galleries and render all events

--- src/app/_components/last-events/last-events.tsx
+++ src/app/_components/last-events/last-events.tsx
@@ -1,21 +1,25 @@
-import { getGalleries } from "@/app/controllers/gallery";
+"use client";
 import LastEventItem from "./_components/last-event-item";
+import { useGalleries } from "@/hooks/useGalleries";
 
-const LastEvents = async () => {
-  const events = await getGalleries({ search: "" });
-  if (events.length === 0) return;
+const LastEvents = () => {
+  const { data: events } = useGalleries();
+  if (!events) return;
   return (
     <div className="flex flex-col gap-2">
       <h2 className="m-auto bg-gradient-to-r from-[#ffb400] to-[#ff4800] bg-clip-text text-center text-5xl font-bold text-transparent">
         Últimos Eventos
       </h2>
-      <LastEventItem
-        date={events[0].date}
-        title={events[0].title}
-        description={events[0].description}
-        location={events[0].location}
-        galeryId={events[0].id}
-      />
+      {events.map((event) => (
+        <LastEventItem
+          key={event.id}
+          date={event.date}
+          title={event.title}
+          description={event.description}
+          location={event.location}
+          galeryId={event.id}
+        />
+      ))}
     </div>
   );
 };
