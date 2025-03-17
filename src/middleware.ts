193d2c0a1c111372dc30fb3cbb301dd31b1238d2
Gabriel Guarini <gabrielrguarini@gmail.com>
2025-03-17 14:52:03 -0300

    feat: temporarily remove authentication

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,20 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
 // const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/", "/login(.*)"]);
 
-const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+// const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
-export default clerkMiddleware(async (auth, req) => {
-  if (isAdminRoute(req)) {
-    const session = await auth();
-    if (!session.userId) {
-      await auth.protect();
-    }
-    const role = session.sessionClaims?.metadata?.role;
-    if (role !== "admin") {
-      return Response.redirect(new URL("/", req.url));
-    }
-  }
+export default clerkMiddleware(async () => {
+  // if (isAdminRoute(req)) {
+  //   const session = await auth();
+  //   if (!session.userId) {
+  //     await auth.protect();
+  //   }
+  //   const role = session.sessionClaims?.metadata?.role;
+  //   if (role !== "admin") {
+  //     return Response.redirect(new URL("/", req.url));
+  //   }
+  // }
 });
 
 export const config = {
