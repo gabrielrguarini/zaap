a0e9e6d96388c2a9df0141fa7b75676af4e3d187
Gabriel Guarini <gabrielrguarini@gmail.com>
2025-03-19 15:17:41 -0300

    refactor: remove create gallery and upload page components

--- src/app/(logged)/admin/create-gallery/page.tsx
+++
@@ -1,9 +0,0 @@
-import CreateGalleryButton from "./create-gallery-button";
-
-export default function createGalleryPage() {
-  return (
-    <div className="p-4">
-      <CreateGalleryButton />
-    </div>
-  );
-}
