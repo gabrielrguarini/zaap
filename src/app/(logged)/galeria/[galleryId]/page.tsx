95549182d05278e5a5eccb1375797d8e9252d206
Gabriel Guarini <gabrielrguarini@gmail.com>
2025-03-19 17:14:05 -0300

    feat: add error handling for non-existent galleries

--- src/app/(logged)/galeria/[galleryId]/page.tsx
+++ src/app/(logged)/galeria/[galleryId]/page.tsx
@@ -3,6 +3,7 @@
 import { SessionProvider } from "next-auth/react";
 import GalleryImages from "./_components/gallery-images";
 import { auth } from "@/auth";
+import { getGalleryById } from "@/app/controllers/gallery";
 
 export default async function GaleryPage({
   params,
@@ -10,7 +11,12 @@ export default async function GaleryPage({
   params: Promise<{ galleryId: string }>;
 }) {
   const { galleryId: galleryId } = await params;
+  const gallery = await getGalleryById(galleryId);
   const session = await auth();
+
+  if (!gallery) {
+    return <h1 className="text-2xl">Galeria não encontrada</h1>;
+  }
   return (
     <SessionProvider session={session}>
       <main className="m-auto mt-4 flex min-h-full w-full max-w-5xl flex-col gap-2 md:gap-4">
