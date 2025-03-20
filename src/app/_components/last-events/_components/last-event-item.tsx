8e9c5a98cec329327874780d5d88907e68cea752
Gabriel Guarini <gabrielrguarini@gmail.com>
2025-03-20 08:47:12 -0300

    feat: adjust LastEventItem style for better alignment and spacing

--- src/app/_components/last-events/_components/last-event-item.tsx
+++ src/app/_components/last-events/_components/last-event-item.tsx
@@ -14,8 +14,8 @@ const LastEventItem = ({
   galeryId: string;
 }) => {
   return (
-    <div className="flex items-center justify-between rounded-xl bg-black px-8 py-2">
-      <div className="flex items-center justify-start gap-1 text-start">
+    <div className="flex items-center justify-between rounded-xl bg-black px-8 py-1">
+      <div className="flex items-center justify-center gap-1 text-start">
         <p className="bg-gradient-to-r from-[#ffb400] to-[#ff4800] bg-clip-text text-center text-3xl font-bold text-transparent">
           {date?.toLocaleDateString("pt-BR", {
             day: "2-digit",
@@ -35,12 +35,12 @@ const LastEventItem = ({
       <p className="min-w-[250px]">
         <span className="font-bold">{type}</span> - {location}
       </p>
-      <div className="rounded-md bg-gradient-to-r from-[#ffb400] to-[#ff4800] p-[2px]">
+      <div className="rounded-md bg-gradient-to-r from-[#ffb400] to-[#ff4800] p-[1px]">
         <div className="rounded-md bg-black px-2">
           <Dialog
             title="Digite o código do evento"
             buttonString="Ver evento"
-            className="bg-transparent hover:bg-transparent"
+            className="bg-transparent py-1 hover:bg-transparent"
           >
             <SeeGallery />
           </Dialog>
