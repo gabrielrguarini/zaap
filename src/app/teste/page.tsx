ca1de58e876ff2145d2ab6f194abb37554b2bb42
Gabriel Guarini <gabrielrguarini@gmail.com>
2025-03-27 13:55:54 -0300

    feat: add Navbar component to Teste page for improved navigation

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { useImages } from "@/hooks/useImages";
 import PhotoGallery from "../_components/GalleryTeste";
+import { Navbar } from "../_components/navbar";
 
 export default function Teste() {
   const { data } = useImages({ galleryId: "TD79TN" });
   if (!data) return null;
   return (
-    <div className="min-h-screen bg-background p-8 text-foreground">
+    <div className="min-h-screen bg-background p-8">
+      <Navbar />
       <h1 className="mb-8 text-3xl font-bold text-primary">
         Galeria de Imagens
       </h1>
