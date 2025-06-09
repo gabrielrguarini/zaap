9ffc5fe66ab9bf584ff5f5fca3bc54e81f58aefc
Gabriel Guarini <gabrielrguarini@gmail.com>
2025-06-09 15:51:14 -0300

    feat: use dynamic id for EditButton in TableRow component

--- src/app/(logged)/admin/_components/table-row.tsx
+++ src/app/(logged)/admin/_components/table-row.tsx
@@ -74,7 +74,7 @@ export const TableRow = ({
         </Dialog>
       </td>
       <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
-        <EditButton id="8TLXXJ" />
+        <EditButton id={id} />
       </td>
       <td className="whitespace-nowrap px-4 py-2 text-gray-700 dark:text-gray-200">
         <Dialog
