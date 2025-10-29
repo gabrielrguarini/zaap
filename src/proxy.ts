03c039e13d40663cf1995ded6e88fc2126b82bce
Gabriel Guarini <gabrielrguarini@gmail.com>
2025-10-29 15:06:45 -0300

    chore: update next to v16

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,4 @@
-export { auth as middleware } from "@/auth";
+export { auth as proxy } from "@/auth";
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|mp4|json|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
