f1c56569987fdc154d703223844021f8ea279b07
Gabriel Guarini <gabrielrguarini@gmail.com>
2025-10-29 14:19:16 -0300

    feat: add qualities to next config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     remotePatterns: [
       { hostname: "zaap-bucket.s3.sa-east-1.amazonaws.com" },
       { hostname: "scontent.cdninstagram.com" },
       { hostname: "**.cdninstagram.com" },
     ],
+    qualities: [20, 30, 75, 100],
   },
 };
 
